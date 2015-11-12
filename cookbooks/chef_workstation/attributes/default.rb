b4dd651bd7ec90154b642aa9e66f0c1a41eb599b
Matt Wrock <matt@mattwrock.com>
2015-11-12 13:26:01 -0800

    updating to chefdk 0.10.0 and vagrant 1.7.4

--- cookbooks/chef_workstation/attributes/default.rb
+++ cookbooks/chef_workstation/attributes/default.rb
@@ -1,8 +1,8 @@
 default["chef_workstation"]["chef_user"] = ENV['USER']
 default["chef_workstation"]["user"] = 'vagrant'
 default["chef_workstation"]["group"] = 'vagrant'
-default['chef_workstation']['chefdk_version'] = '0.7.0-1'
-default['chef_workstation']['vagrant_version'] = '1.7.2'
+default['chef_workstation']['chefdk_version'] = '0.10.0-1'
+default['chef_workstation']['vagrant_version'] = '1.7.4'
 default['chef_workstation']['docker_version'] = '1.6.0'
 default['chef_workstation']['chef_server']['user'] = "\#{ENV['USERNAME'] || ENV['USER']}"
 default['chef_workstation']['chef_server']['url'] = "https://api.opscode.com/organizations/"
